e5fdcccb8bebe50b61d879347be869de759e97ee
neel-s-g <neelmani.sgautam@gmail.com>
2023-05-18 16:49:20 +0530

    Test Commit 12

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
 	@GetMapping("/t")
 	public String index() {
 		log.info("Test 11");
-		log.info("Test 2");
+		log.info("Test 12");
 		log.info("Test 3");
 		return "Greetings from Spring Boot with Demo Unified Flow!";
 	}
